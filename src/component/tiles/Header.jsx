e972a22c394077a0c6a30dde760effb8fa9b1a6b
gbkim <gb.kim@fusionsoft.co.kr>
2023-05-19 17:51:07 +0900

    commit

---
+++ src/component/tiles/Header.jsx
@@ -0,0 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default function Header() {
+    return (
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Gabin</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">todos</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">login</a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+                </nav>
+    )
+}
